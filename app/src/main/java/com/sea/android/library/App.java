0931e94bfe64558a59ad55231fc7368dfa651415
chuwuwang <chuwuwang@126.com>
2022-05-22 14:43:11 +0800

    feat：删除hook模块

--- app/src/main/java/com/sea/android/library/App.java
+++ app/src/main/java/com/sea/android/library/App.java
@@ -1,43 +1,15 @@
 package com.sea.android.library;
 
 import android.app.Application;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.widget.ImageView;
-
-import com.sea.hook.MethodHook;
-import com.sea.hook.MethodParameter;
-import com.sea.hook.PHook;
-import com.sea.library.common.logger.Logger;
 
 public class App extends Application {
 
     public static final String TAG = "App";
-    public static final String FILE_AUTHORITIES = BuildConfig.APPLICATION_ID +  ".provider";
+    public static final String FILE_AUTHORITIES = BuildConfig.APPLICATION_ID + ".provider";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        hook();
-    }
-
-    private static void hook() {
-        MethodHook methodHook = new MethodHook() {
-
-            @Override
-            public void beforeHookedMethod(MethodParameter param) throws Throwable {
-                Logger.e(TAG, "beforeHookedMethod param: " + param);
-            }
-
-            @Override
-            public void afterHookedMethod(MethodParameter param) throws Throwable {
-                Logger.e(TAG, "afterHookedMethod param: " + param);
-            }
-
-        };
-        PHook.findAndHookMethod(ImageView.class, "setImageBitmap", Bitmap.class, methodHook);
-        PHook.findAndHookMethod(ImageView.class, "setImageResource", Integer.class, methodHook);
-        PHook.findAndHookMethod(ImageView.class, "setImageDrawable", Drawable.class, methodHook);
     }
 
 }
--- hook/src/main/java/com/sea/hook/MethodHook.java
+++
@@ -1,9 +0,0 @@
-package com.sea.hook;
-
-public interface MethodHook {
-
-    void beforeHookedMethod(MethodParameter param) throws Throwable;
-
-    void afterHookedMethod(MethodParameter param) throws Throwable;
-
-}
